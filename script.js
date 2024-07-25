1327f7f9b8c19d98b55a805ac4a214f1c3b52af4
KOTLIN <acarolinavilel@gmail.com>
2024-07-25 03:00:55 -0300

    22

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 
+AOS.init();
 
-
+$(document).ready(function() {
+  $(window).on('scroll', function() {
+      if ($(this).scrollTop() > 50) {
+          $('.menu-desktop').addClass('active');
+      } else {
+          $('.menu-desktop').removeClass('active');
+      }
+  });
+});
 var elem = document.querySelector('.sidenav');
 var instance = new M.Sidenav(elem);
 
@@ -10,26 +19,46 @@ $(document).ready(function(){
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-  // Inicializa o carousel do Materialize
-  var elems = document.querySelectorAll('.carousel');
-  var instances = M.Carousel.init(elems, {
-     
+  // Função para inicializar o carousel
+  function initializeCarousel(padding) {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {
       indicators: false,
-      padding: -250,
+      padding: padding,
       opacity: false,
-  });
+    });
 
-  // Obtém a instância do carousel
-  var carouselInstance = M.Carousel.getInstance(document.querySelector('.carousel'));
+    // Obtém a instância do carousel
+    var carouselInstance = M.Carousel.getInstance(document.querySelector('.carousel'));
 
-  // Configura as setas personalizadas
-  document.querySelector('.carousel-prev').addEventListener('click', function() {
+    // Configura as setas personalizadas
+    document.querySelector('.carousel-prev').addEventListener('click', function() {
       carouselInstance.prev();
-  });
+    });
 
-  document.querySelector('.carousel-next').addEventListener('click', function() {
+    document.querySelector('.carousel-next').addEventListener('click', function() {
       carouselInstance.next();
-  });
+    });
+  }
+
+  // Função para verificar o tamanho da tela e atualizar o padding
+  function updateCarouselPadding() {
+
+    if (window.innerWidth < 1000){
+      var padding = -300
+    }
+    else{
+      var padding = 200
+    }
+   
+    initializeCarousel(padding);
+  }
+
+  // Atualiza o padding na carga inicial
+  updateCarouselPadding();
+
+  // Atualiza o padding sempre que a janela é redimensionada
+  window.addEventListener('resize', updateCarouselPadding);
 });
 
   // Inicializa o Slick Slider
